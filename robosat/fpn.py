8876768ffdd5a5b6ed1ad6eec0b9b0e96a1f77ec
Bhargav Kowshik <bhargav@mapbox.com>
2018-08-10 10:02:55 +0530

    Assert image resolution has to be divisible by 32 for resnet in fpn

--- robosat/fpn.py
+++ robosat/fpn.py
@@ -46,6 +46,9 @@ class FPN(nn.Module):
     def forward(self, x):
         # Bottom-up pathway, from ResNet
 
+        size = x.size()
+        assert size[-1] % 32 == 0 and size[-2] % 32 == 0, "image resolution has to be divisible by 32 for resnet"
+
         enc0 = self.resnet.conv1(x)
         enc0 = self.resnet.bn1(enc0)
         enc0 = self.resnet.relu(enc0)
