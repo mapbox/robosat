5ade9f2734c765dd535d3d02f6d7039c6ab9ef53
ocourtin <o@courtin.co>
2018-11-13 09:15:35 +0100

    allow to unzoom, by providing a vector grid

--- robosat/utils.py
+++ robosat/utils.py
@@ -1,6 +1,8 @@
 import re
 import os 
 from robosat.tiles import pixel_to_location
+import mercantile
+import json
 import matplotlib
 
 matplotlib.use("Agg")
@@ -27,9 +29,12 @@ def plot(out, history):
 
 def leaflet(out, base_url, tiles, ext):
 
+    grid = json.dumps([mercantile.feature(tile) for tile in tiles])
+
     leaflet = open("./robosat/tools/templates/leaflet.html", "r").read()
     leaflet = re.sub("{{base_url}}", base_url, leaflet)
     leaflet = re.sub("{{ext}}", ext, leaflet)
+    leaflet = re.sub("{{grid}}", grid, leaflet)
 
     # Could surely be improve, but for now, took the first tile to center on
     tile = (list(tiles)[0])
