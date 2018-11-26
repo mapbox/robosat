0433e26722cf138d2f2fde04297406e35af32462
ocourtin <o@courtin.co>
2018-11-26 16:31:25 +0100

    Use a single type parameter, more user understandable prototype

--- robosat/tools/cover.py
+++ robosat/tools/cover.py
@@ -18,9 +18,9 @@ def add_parser(subparser):
     )
 
     parser.add_argument("--zoom", type=int, help="zoom level of tiles")
-    parser.add_argument("--features", type=str, help="path to GeoJSON features")
-    parser.add_argument("--bbox", type=str, help="bbox expressed in lat/lon (i.e EPSG:4326)")
-    parser.add_argument("--path", type=str, help="Slippy Map directory input path")
+    parser.add_argument("--type", type=str, default="geojson", choices=["geojson", "bbox", "dir"], help="input type")
+    help = "input value, upon type either: a geojson file path, a bbox in lat/lon ESPG:4326, or a slippymap dir path"
+    parser.add_argument("input", type=str, help=help)
     parser.add_argument("out", type=str, help="path to csv file to store tiles in")
 
     parser.set_defaults(func=main)
@@ -28,27 +28,26 @@ def add_parser(subparser):
 
 def main(args):
 
-    if not args.zoom and (args.features or args.bbox):
+    if not args.zoom and args.type in ["geojson", "bbox"]:
         sys.exit("Zoom parameter is mandatory")
 
     cover = []
 
-    if args.features:
-        with open(args.features) as f:
+    if args.type == "geojson":
+        with open(args.input) as f:
             features = json.load(f)
 
         for feature in tqdm(features["features"], ascii=True, unit="feature"):
             cover.extend(map(tuple, burntiles.burn([feature], args.zoom).tolist()))
 
-        # tiles can overlap for multiple features; unique tile ids
-        cover = list(set(cover))
+        cover = list(set(cover))  # tiles can overlap for multiple features; unique tile ids
 
-    elif args.bbox:
-        west, south, east, north = map(float, args.bbox.split(","))
+    elif args.type == "bbox":
+        west, south, east, north = map(float, args.input.split(","))
         cover = tiles(west, south, east, north, args.zoom)
 
-    elif args.path:
-        cover = [tile for tile, _ in tiles_from_slippy_map(args.path)]
+    elif args.type == "dir":
+        cover = [tile for tile, _ in tiles_from_slippy_map(args.input)]
 
     else:
         sys.exit("You have to provide either a GeoJson features file, or a lat/lon bbox, or an input directory path")
