1a451d3a2d05ac4908c56b4557e071a3331b58e2
ocourtin <o@courtin.co>
2018-11-27 01:29:36 +0100

    few cleanup. Add path parameter to allow an user to set his own extension dir

--- robosat/tools/extract.py
+++ robosat/tools/extract.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import argparse
+
 import pkgutil
 from pathlib import Path
 from importlib import import_module
@@ -14,6 +15,7 @@ def add_parser(subparser):
     )
 
     parser.add_argument("--type", type=str, required=True, help="type of feature to extract")
+    parser.add_argument("--path", type=str, help="path to user's extension modules dir")
     parser.add_argument("pbf", type=str, help="path to .osm.pbf base map")
     parser.add_argument("out", type=str, help="path to GeoJSON file to store features in")
 
@@ -21,16 +23,18 @@ def add_parser(subparser):
 
 
 def main(args):
-    module_path_search = [os.path.join(Path(__file__).parent.parent, "osm")]
-    modules = [name for _, name, _ in pkgutil.iter_modules(module_path_search) if name != "core"]
-    if args.type not in modules:
-        sys.exit("Unknown type, thoses available are {}".format(modules))
+    module_search_path = [args.path] if args.path else []
+    module_search_path.append(os.path.join(Path(__file__).parent.parent, "osm"))
+    modules = [(path, name) for path, name, _ in pkgutil.iter_modules(module_search_path) if name != "core"]
+    if args.type not in [name for _, name in modules]:
+        sys.exit("Unknown type, thoses available are {}".format([name for _, name in modules]))
 
-    try:
-        module = import_module("robosat.osm." + args.type, package=__name__)
-        handler = getattr(module, "{}Handler".format(args.type.title()))
-        handler().apply_file(filename=args.pbf, locations=True)
-    except:
-        sys.exit("Something get wrong, unable to call {}Handler", args.type.title())
+    if args.path:
+        sys.path.append(args.path)
+        module = import_module(args.type)
+    else:
+        module = import_module("robosat.osm.{}".format(args.type))
 
-    handler().save(args.out)
+    handler = getattr(module, "{}Handler".format(args.type.title()))()
+    handler.apply_file(filename=args.pbf, locations=True)
+    handler.save(args.out)
--- robosat/tools/features.py
+++ robosat/tools/features.py
@@ -2,11 +2,14 @@ import os
 import sys
 import argparse
 from tqdm import tqdm
+
 import numpy as np
 from PIL import Image
+
 import pkgutil
 from pathlib import Path
 from importlib import import_module
+
 from robosat.config import load_config
 from robosat.tiles import tiles_from_slippy_map
 
@@ -20,6 +23,7 @@ def add_parser(subparser):
 
     parser.add_argument("--type", type=str, required=True, help="type of feature to extract")
     parser.add_argument("--dataset", type=str, required=True, help="path to dataset configuration file")
+    parser.add_argument("--path", type=str, help="path to user's extension modules dir")
     parser.add_argument("masks", type=str, help="slippy map directory with segmentation masks")
     parser.add_argument("out", type=str, help="path to GeoJSON file to store features in")
 
@@ -27,27 +31,30 @@ def add_parser(subparser):
 
 
 def main(args):
+
+    module_search_path = [args.path] if args.path else []
+    module_search_path.append(os.path.join(Path(__file__).parent.parent, "features"))
+    modules = [(path, name) for path, name, _ in pkgutil.iter_modules(module_search_path) if name != "core"]
+    if args.type not in [name for _, name in modules]:
+        sys.exit("Unknown type, thoses available are {}".format([name for _, name in modules]))
+
     dataset = load_config(args.dataset)
-
-    module_path_search = [os.path.join(Path(__file__).parent.parent, "features")]
-    modules = [name for _, name, _ in pkgutil.iter_modules(module_path_search) if name != "core"]
-    if args.type not in modules:
-        sys.exit("Unknown type, thoses available are {}".format(modules))
-
     labels = dataset["common"]["classes"]
     if args.type not in labels:
         sys.exit("The type you asked is not consistent with yours classes in the dataset file provided.")
     index = labels.index(args.type)
 
-    try:
-        module = import_module("robosat.features." + args.type, package=__name__)
-        handler = getattr(module, "{}Handler".format(args.type.title()))
-    except:
-        sys.exit("Something get wrong, unable to call {}Handler", args.type.title())
+    if args.path:
+        sys.path.append(args.path)
+        module = import_module(args.type)
+    else:
+        module = import_module("robosat.features.{}".format(args.type))
+
+    handler = getattr(module, "{}Handler".format(args.type.title()))()
 
     for tile, path in tqdm(list(tiles_from_slippy_map(args.masks)), ascii=True, unit="mask"):
         image = np.array(Image.open(path).convert("P"), dtype=np.uint8)
         mask = (image == index).astype(np.uint8)
-        handler().apply(tile, mask)
+        handler.apply(tile, mask)
 
-    handler().save(args.out)
+    handler.save(args.out)
